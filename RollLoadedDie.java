e51efa5a266647f13c30274c118ee842161c38bb
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-01-12 12:18:19 -0600

    should be finished

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -1,5 +1,21 @@
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 public class RollLoadedDie {
     public static void main(String[] args) {
-        
+        Double test = Math.random();
+        if(test <0.126){
+            System.out.println("There is a dot on the die, one.");
+        }else if(test <0.251){
+            System.out.println("There is 2 dots on the die, two.");
+        }else if (test < 0.376){
+            System.out.println("You see three dots on the die, 3.");
+        }else if (test <0.501){
+            System.out.println("There is 4 dots on the die, four.");
+        }else if (test <0.626){
+            System.out.println("You see five dots on the die, 5.");
+        }else {
+            System.out.println("You see six dots on the die, 6.");
+        }
     }
 }
+// javac RollLoadedDie.java; java RollLoadedDie 
