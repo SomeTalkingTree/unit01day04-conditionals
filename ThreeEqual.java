dd2ee6e570e911b353b6bac0b7a5239a50b4d7b2
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-01-13 12:23:24 -0600

    done

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -7,7 +7,7 @@ public class ThreeEqual {
             int num = Integer.parseInt(args[0]);
             int num1 = Integer.parseInt(args[1]);
             int num2 = Integer.parseInt(args[2]);
-            if (num == num1 & num == num2 & num1 == num2) {
+            if (num == num1 && num == num2 && num1 == num2) {
                 System.out.println("This is all equal");
             } else if(num != num1 || num1 != num2 || num != num2){
                 if(num == num1 & num1 !=num2){
