f342290c1979a1c40976dc72ba0ad4926f3db6de
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-01-13 11:19:11 -0600

    changed some stuff but its small

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -5,13 +5,13 @@ public class RollLoadedDie {
         Double test = Math.random();
         if(test <0.126){
             System.out.println("There is a dot on the die, one.");
-        }else if(test <0.251){
+        }else if(test <=0.25){
             System.out.println("There is 2 dots on the die, two.");
-        }else if (test < 0.376){
+        }else if (test <= 0.375){
             System.out.println("You see three dots on the die, 3.");
-        }else if (test <0.501){
+        }else if (test <=0.50){
             System.out.println("There is 4 dots on the die, four.");
-        }else if (test <0.626){
+        }else if (test <=0.625){
             System.out.println("You see five dots on the die, 5.");
         }else {
             System.out.println("You see six dots on the die, 6.");
